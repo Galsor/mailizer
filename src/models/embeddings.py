4b8f3db1484e6a1dce02eb16fa18443ca117e68e
Antoine Meilliez <meilliez.antoine@gmail.com>
2021-02-08 19:05:23 +0100

    Add dummy models

---
+++ src/models/embeddings.py
@@ -0,0 +1,3 @@
+from melusine.nlp_tools.embedding import Embedding
+
+pretrained_embedding = Embedding(input_column='clean_body', min_count=3)
---
+++ src/models/keywords.py
@@ -0,0 +1,4 @@
+# Summarizer
+from melusine.summarizer.keywords_generator import KeywordsGenerator
+
+keywords_generator = KeywordsGenerator(n_max_keywords=3)
