d4c4ab78c9b1f2e6ecaab264447efdff0b940053
Antoine Meilliez <meilliez.antoine@gmail.com>
2021-02-11 12:54:32 +0100

    Conda env + message

--- activate.sh
+++ activate.sh
@@ -1,1 +1,19 @@
-source .mailizer/bin/activate
+function activate_venv() {
+{ # try: activate from the conda environment:
+        conda activate mailizer
+    } || { # catch: error
+        echo ""; echo "ERROR: failed to activate virtual environment .venv! do it yourself"; return 1
+    }
+}
+
+activate_venv && (
+    echo ""
+    echo "************************************************************************************"
+    echo "Successfuly activated the virtual environment; you are now using this python:"
+    echo "$ which python"
+    echo "$(which python)"
+    echo "************************************************************************************"
+    echo ""
+)
+export PYTHONPATH="$(pwd):$PYTHONPATH"
+export JUPYTER_PATH="$(pwd):$JUPYTER_PATH"
